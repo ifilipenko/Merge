a26a84ef4ade98504a9909383bf0163fe5e9a961
Filipenko Innokentiy <filipenko.in@gmail.com>
2013-03-12 15:08:37 +0600

    Fix args resolvoing

--- src/Difference/Parameters.cs
+++ src/Difference/Parameters.cs
@@ -6,13 +6,13 @@ namespace Merge
     {
         public string FilePath1 { get; set; }
         public string FilePath2 { get; set; }
-        public string FilePath3 { get; set; }
+        public string OriginalFilePath { get; set; }
         public bool Merge { get; set; }
         public bool DiffOnly { get; set; }
 
         public bool IsInitialized
         {
-            get { return !string.IsNullOrWhiteSpace(FilePath1) && string.IsNullOrWhiteSpace(FilePath2); }
+            get { return !string.IsNullOrWhiteSpace(FilePath1) && !string.IsNullOrWhiteSpace(OriginalFilePath); }
         }
 
         public void SetFiles(string[] files)
@@ -20,13 +20,13 @@ namespace Merge
             switch (files.Length)
             {
                 case 2:
-                    FilePath1 = files[0];
-                    FilePath2 = files[1];
+                    OriginalFilePath = files[0];
+                    FilePath1 = files[1];
                     break;
                 case 3:
-                    FilePath1 = files[0];
-                    FilePath2 = files[1];
-                    FilePath3 = files[2];
+                    OriginalFilePath = files[0];
+                    FilePath1 = files[1];
+                    FilePath2 = files[2];
                     break;
                 default:
                     throw new ArgumentException("Unexpected files list length", "files");
