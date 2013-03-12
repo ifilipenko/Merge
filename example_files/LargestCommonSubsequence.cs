2a1d997f0683831d346f0f5ec81a98166e7eb189
Filipenko Innokentiy <filipenko.in@gmail.com>
2013-03-12 15:07:27 +0600

    Add example files

---
+++ example_files/LargestCommonSubsequence.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BuildingBlocks.Common;
+
+namespace Merge
+{
+    public class LargestCommonSubsequence<T>
+    {
+        public class Matrix
+        {
+            private readonly int[,] _data;
+
+            public Matrix(int sequence1Length, int sequence2Length)
+            {
+                _data = new int[sequence1Length + 1, sequence2Length + 1];
+            }
+
+            public int MaxLength { get; private set; }
+
+            public bool IsEmpty
+            {
+                get { return Sequence1Dimension == 1 || Sequence2Dimension == 1; }
+            }
+
+            public int Sequence1Dimension
+            {
+                get { return _data.GetLength(0); }
+            }
+
+            public int Sequence2Dimension
+            {
+                get { return _data.GetLength(0); }
+            }
+
+            public int this[int i, int j]
+            {
+                get { return _data[i, j]; }
+                set
+                {
+                    _data[i, j] = value;
+                    if (MaxLength < value)
+                    {
+                        MaxLength = value;
+                    }
+                }
+            }
+        }
+
+        private readonly T[] _sequence1;
+        private readonly T[] _sequence2;
+        private readonly IEqualityComparer<T> _equalityComparer;
+
+        public LargestCommonSubsequence(T[] sequence1, T[] sequence2, IEqualityComparer<T> equalityComparer = null)
+        {
+            _sequence1 = sequence1;
+            _sequence2 = sequence2;
+            _equalityComparer = equalityComparer ?? new DefaultEqualityComparer<T>();
+        }
+
+        /// <summary>
+        /// Implementation algorithm described in
+        /// http://www.algorithmist.com/index.php/Longest_Common_Subsequence
+        /// </summary>
+        /// <returns></returns>
+        public Matrix CreateMatrix()
+        {
+            var matrix = new Matrix(_sequence1.Length, _sequence2.Length);
+            if (matrix.IsEmpty)
+                return matrix;
+
+            for (int i = 0; i <= _sequence1.Length; i++)
+            {
+                for (int j = 0; j <= _sequence2.Length; j++)
+                {
+                    if (j == 0 || i == 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                    else if (_equalityComparer.Equals(_sequence1[i - 1], _sequence2[j - 1]))
+                    {
+                        matrix[i, j] = matrix[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = Math.Max(matrix[i - 1, j], matrix[i, j - 1]);
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
+        /// <summary>
+        /// Implementation algorithm described in
+        /// http://www.algorithmist.com/index.php/Longest_Common_Subsequence
+        /// </summary>
+        /// <returns></returns>
+        public T[] GetSubsequence()
+        {
+            var matrix = CreateMatrix();
+            return GetSubsequenceCore(matrix, _sequence1.Length, _sequence2.Length).ToArray();
+        }
+
+        /// <summary>
+        /// Implementation algorithm described in 
+        /// http://en.wikipedia.org/wiki/Longest_common_subsequence_problem
+        /// </summary>
+        /// <typeparam name="TResult">Processing result</typeparam>
+        /// <param name="processEquals">processing function identical items from both sequences</param>
+        /// <param name="processAdded">processing function added items to sequence2</param>
+        /// <param name="processDeleted">processing function deleted items from sequence1</param>
+        /// <returns></returns>
+        public IEnumerable<TResult> Backtrack<TResult>(Func<T, T, TResult> processEquals,
+                                                       Func<T, TResult> processAdded,
+                                                       Func<T, TResult> processDeleted)
+        {
+            var matrix = CreateMatrix();
+            return BacktrackCore(matrix, _sequence1.Length, _sequence2.Length, processEquals, processAdded, processDeleted).ToArray();
+        }
+
+        private IEnumerable<TResult> BacktrackCore<TResult>(Matrix matrix, int i, int j,
+                                                            Func<T, T, TResult> processEquals,
+                                                            Func<T, TResult> processAdded,
+                                                            Func<T, TResult> processDeleted)
+        {
+            if (i > 0 && j > 0 && _equalityComparer.Equals(_sequence1[i-1], _sequence2[j-1]))
+            {
+                var result = BacktrackCore(matrix, i - 1, j - 1, processEquals, processAdded, processDeleted);
+                return result.Concat(processEquals(_sequence1[i - 1], _sequence2[j - 1]).ToEnumerable());
+            }
+            if (j > 0 && (i == 0 || matrix[i, j - 1] >= matrix[i - 1, j]))
+            {
+                var result = BacktrackCore(matrix, i, j - 1, processEquals, processAdded, processDeleted);
+                return result.Concat(processAdded(_sequence2[j - 1]).ToEnumerable());
+            }
+            if (i > 0 && (j == 0 || matrix[i, j - 1] < matrix[i - 1, j]))
+            {
+                var result = BacktrackCore(matrix, i - 1, j, processEquals, processAdded, processDeleted);
+                return result.Concat(processDeleted(_sequence1[i - 1]).ToEnumerable());
+            }
+            return Enumerable.Empty<TResult>();
+        }
+
+        private IEnumerable<T> GetSubsequenceCore(Matrix matrix, int i, int j)
+        {
+            if (i == 0 || j == 0)
+                return Enumerable.Empty<T>();
+
+            if (_equalityComparer.Equals(_sequence1[i - 1], _sequence2[j - 1]))
+            {
+                var subsequence = GetSubsequenceCore(matrix, i - 1, j - 1);
+                return subsequence.Concat(_sequence1[i - 1].ToEnumerable());
+            }
+
+            return matrix[i - 1, j] > matrix[i, j - 1]
+                       ? GetSubsequenceCore(matrix, i - 1, j)
+                       : GetSubsequenceCore(matrix, i, j - 1);
+        }
+    }
+}
---
+++ example_files/test_diff_to_file.bat
@@ -0,0 +1,2 @@
+del test.diff
+..\src\Difference\bin\Debug\Merge.exe -diff "Algorithms_changed.cs" "Algorithms_changed2.cs" >> test.diff
